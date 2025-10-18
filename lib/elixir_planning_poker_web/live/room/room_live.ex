25b8ae8e840d3e913b4953e2ef70e2cd94614889
augusto <augustosouzas01@gmail.com>
2025-10-18 17:35:34 -0300

    redirect and flash when room not found

--- lib/elixir_planning_poker_web/live/room/room_live.ex
+++ lib/elixir_planning_poker_web/live/room/room_live.ex
@@ -1,12 +1,25 @@
 defmodule ElixirPlanningPokerWeb.RoomLive do
   use ElixirPlanningPokerWeb, :live_view
 
-  def mount(%{"room_code" => room_code}, session, socket) do
-    socket
-    |> assign(:user_token, session["user_token"])
-    |> assign(:room_code, room_code)
-    {:ok, assign(socket, :room_code, room_code)}
-  end
+def mount(%{"room_code" => room_code}, session, socket) do
+  socket =
+    case ElixirPlanningPoker.RoomManager.get_state(room_code) do
+      {:ok, state} ->
+        IO.inspect(state, label: "Room State")
+        socket
+
+      {:error, :not_found} ->
+        socket
+        |> put_flash(:error, "Room not found.")
+        |> push_navigate(to: "/")
+
+    end
+
+  {:ok,
+   socket
+   |> assign(:user_token, session["user_token"])
+   |> assign(:room_code, room_code)}
+end
 
 def handle_event("teste", _params, socket) do
   case ElixirPlanningPoker.RoomManager.get_state(socket.assigns.room_code) do
